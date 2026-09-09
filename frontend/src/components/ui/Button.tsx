492c7a5fa7d545a42c53d5edc9c5e1666cd205d9
DANIEL DE CAMPOS MEDEIROS <25162386@edu119.local>
2026-09-09 13:07:04 -0300

    alterações no frontend

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,2 +1,4 @@
 import type { ButtonHTMLAttributes, PropsWithChildren } from 'react';
-export function Button({children,...props}:PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>){return <button className="btn" {...props}>{children}</button>}
+
+export function Button({children,...props}:PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>)
+{return <button className="btn" {...props}>{children}</button>}
--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -1,2 +1,4 @@
 import type { PropsWithChildren } from 'react';
-export function Card({children}:PropsWithChildren){return <section className="card">{children}</section>}
+
+export function Card({children}:PropsWithChildren)
+{return <section className="card">{children}</section>}
